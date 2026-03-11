4797ffcf13c3b97f8a082c753fad8dcb2e7512d3
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2026-03-11 20:27:41 -0300

    modelo fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,9 @@
 import sys
+# int fibonacci (int n)
+# retorna o n-ésimo número da sequência de fibonacci
+# erro: Ns iguais ou menores a 0
+# condições de parada: 1 e 2 retornam 1
+
 
 def fibonacci(n):
   if n <= 0: raise Exception('Número inexistente na sequência de Fibonacci')
